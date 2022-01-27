c69a31a002736053305617b097fa8d1ca710dcef
Mivisha <90041857+Mivisha@users.noreply.github.com>
2022-01-27 22:27:32 +0200

    Add Destructuring

--- logs.js
+++ logs.js
@@ -1,16 +1,17 @@
 function generateLog(firstPerson,secondPerson) {
-    
+    const {name,damageHP}=firstPerson;
+    const {name:name2}=secondPerson;
     const logs = [
-        `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага.${firstPerson.damageHP}`,
-        `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага.${firstPerson.damageHP}`,
-        `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил.${firstPerson.damageHP}`,
-        `${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар.${firstPerson.damageHP}`,
-        `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника.${firstPerson.damageHP}`,
-        `${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар.${firstPerson.damageHP}`,
-        `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар.${firstPerson.damageHP}`,
-        `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника${firstPerson.damageHP}`,
-        `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника.${firstPerson.damageHP}`,
-        `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику.${firstPerson.damageHP}`
+        `${name} вспомнил что-то важное, но неожиданно ${name2}, не помня себя от испуга, ударил в предплечье врага.${damageHP}`,
+        `${name} поперхнулся, и за это ${name2} с испугу приложил прямой удар коленом в лоб врага.${damageHP}`,
+        `${name} забылся, но в это время наглый ${name2}, приняв волевое решение, неслышно подойдя сзади, ударил.${damageHP}`,
+        `${name} пришел в себя, но неожиданно ${name2} случайно нанес мощнейший удар.${damageHP}`,
+        `${name} поперхнулся, но в это время ${name2} нехотя раздробил кулаком \<вырезанно цензурой\> противника.${damageHP}`,
+        `${name} удивился, а ${name2} пошатнувшись влепил подлый удар.${damageHP}`,
+        `${name} высморкался, но неожиданно ${name2} провел дробящий удар.${damageHP}`,
+        `${name} пошатнулся, и внезапно наглый ${name2} беспричинно ударил в ногу противника${damageHP}`,
+        `${name} расстроился, как вдруг, неожиданно ${name2} случайно влепил стопой в живот соперника.${damageHP}`,
+        `${name} пытался что-то сказать, но вдруг, неожиданно ${name2} со скуки, разбил бровь сопернику.${damageHP}`
     ];
     return logs [random(logs.length)-1];
 }
