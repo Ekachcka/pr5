7b9bd1e0d7bdb925e429a8a9df7d79d9ce3fa85f
Ekachcka <2331286yan@gmail.com>
2022-02-03 12:05:13 +0200

    add 5-5

--- Pokemon.js
+++ Pokemon.js
@@ -1,83 +1,54 @@
-// export const character = {
-//     name: 'Pikachu',
-//     type: 'electric',
-//     defaultHP: 100,
-//     damageHP: 100,
-//     elHP: document.getElementById('health-character'),
-//     elProgressbar: document.getElementById('progressbar-character'),
+class Selectors {
+    constructor(name) {
+        this.elHP = document.getElementById(`health-${name}`);
+        this.elProgressbar = document.getElementById(`progressbar-${name}`);
+    }
+}
 
-//     greet: function(){
-//         return 'Hi, my name is ' + character.name;
-//     },   
-    
-//     renderHPLife: function(){
-//         const $character = document.getElementById('health-character');
-//         console.log($character.innerText);
-//         $character.innerText=character.damageHP+'/'+ character.defaultHP;
-//     },    
-//     renderHPLife: function(){
-//         character.elHP.innerText=character.damageHP + ' / ' + character.defaultHP;
-//     },    
-//     renderProgressbarHP: function(){
-//         character.elProgressbar.style.width = character.damageHP + '%';
-//     },
-   
-//     changeHP: function(count) {
-//         if(character.damageHP<count) {
-//             character.damageHP=0;
-//             alert('Бедный '+ character.name+' проиграл бой!');
-//             $btn.disabled = character;
-//         }else{
-//             character.damageHP -= count;
-//         }
-//         renderHP();
+class Pokemon extends Selectors {
+    constructor({ name, hp, type, selectors }) {
+        super(selectors);
 
-//         const log = character === character ? generateLog(character, enemy) : generateLog(character, character);
-//         const $p = document.createElement('p');
-//         $p.innerText=log;
-//         $logs.appendChild($p);
-//     }  
-// }
-// export const enemy = {
-//     name: 'Charmander',
-//     defaultHP: 100,
-//     damageHP: 100,
-//     elHP: document.getElementById('health-enemy'),
-//     elProgressbar: document.getElementById('progressbar-enemy'),
-   
-   
-//     renderHPLife: function(){
-//         const $character = document.getElementById('health-character');
-//         console.log($character.innerText);
-//         $character.innerText=enemy.damageHP+'/'+ enemy.defaultHP;
-//     },    
-//     renderHPLife: function(){
-//         enemy.elHP.innerText=enemy.damageHP + ' / ' + enemy.defaultHP;
-//     },    
-//     renderProgressbarHP: function(){
-//         enemy.elProgressbar.style.width = enemy.damageHP + '%';
-//     },
-   
-//     changeHP: function(count) {
-//         if(enemy.damageHP<count) {
-//             enemy.damageHP=0;
-//             alert('Бедный '+ enemy.name+' проиграл бой!');
-//             $btn2.disabled = enemy;
+        this.name = name;
+        this.hp = {
+            current: hp,
+            total: hp,
+        };
+        this.type = type;
 
-//         }else{
-//             enemy.damageHP -= count;
-//         }
-//         renderHP();
+        this.renderHP();
+    }
 
-//         const log = enemy === enemy ? generateLog(enemy, character) : generateLog(enemy, enemy);
-//         const $p = document.createElement('p');
-//         $p.innerText=log;
-//         $logs.appendChild($p);
-//     }  
-// }
-// function renderHP(){
-//     character.renderHPLife();
-//     character.renderProgressbarHP();
-//     enemy.renderHPLife();
-//     enemy.renderProgressbarHP();
-// };
+    changeHP = (count) => {
+        this.hp.current -= count;
+
+        if (this.hp.current <= 0) {
+            this.hp.current = 0;
+            alert(this.name + ' проиграл!');
+        }
+
+        this.renderHP();
+
+        return count;
+    }
+
+    renderHPLife = () => {
+        const { elHP, hp: { current, total } } = this;
+
+        elHP.textContent = current + ' / ' + total;
+    }
+
+    renderProgressbarHP = () => {
+        const { hp: { current, total }, elProgressbar } = this;
+        const procent = (current / total) * 100;
+
+        elProgressbar.style.width = procent + '%';
+    }
+
+    renderHP = () => {
+        this.renderHPLife();
+        this.renderProgressbarHP();
+    }
+}
+
+export default Pokemon;
--- main.js
+++ main.js
@@ -1,118 +1,107 @@
-//geroi
-const $btn = document.getElementById('btn-kick');
-const $btn2 = document.getElementById('btn-kick2');
-const $logs = document.querySelector('#logs');
-let SH1 = 0, SH2 = 0;
-let Kol1 = 6, Kol2 = 6;
-const random = (num) => Math.ceil(Math.random() * num);
+import Pokemon from './Pokemon.js';
+import random from "./utils.js";
 
-function stop(count = 6,el) {
-    const innerText=el.innerText;
-    el.innerText=`${innerText} (${count})`;
-    return function () {
-        count--;
-        if (count===0) {
-            el.disabled
-        }
-        el.innerText=`${innerText} (${count})`;
-        return count;
-    }
-}
 
-const character = {
-    name: 'Pikachu',
+const player1 = new Pokemon({
+    name: 'Pickachu',
+    hp: 500,
     type: 'electric',
-    defaultHP: 100,
-    damageHP: 100,
-    elHP: document.getElementById('health-character'),
-    elProgressbar: document.getElementById('progressbar-character'),
+    selectors: 'character',
+});
 
-    greet: function () {
-        return 'Hi, my name is ' + character.name;
-    },
-
-    renderHPLife: function () {
-        const $character = document.getElementById('health-character');
-        console.log($character.innerText);
-        $character.innerText = character.damageHP + '/' + character.defaultHP;
-    },
-    renderHPLife: function () {
-        character.elHP.innerText = character.damageHP + ' / ' + character.defaultHP;
-    },
-    renderProgressbarHP: function () {
-        character.elProgressbar.style.width = character.damageHP + '%';
-    },
-
-    changeHP: function (count) {
-        if (character.damageHP < count) {
-            character.damageHP = 0;
-            alert('Бедный ' + character.name + ' проиграл бой!');
-            $btn.disabled = character;
-        } else {
-            character.damageHP -= count;
-        }
-        renderHP();
-
-        const log = character === character ? generateLog(character, enemy) : generateLog(character, character);
-        const $p = document.createElement('p');
-        $p.innerText = log;
-        $logs.appendChild($p);
-    }
-}
-const enemy = {
+const player2 = new Pokemon({
     name: 'Charmander',
-    defaultHP: 100,
-    damageHP: 100,
-    elHP: document.getElementById('health-enemy'),
-    elProgressbar: document.getElementById('progressbar-enemy'),
+    hp: 400,
+    type: 'fire',
+    selectors: 'enemy',
+});
 
-
-    renderHPLife: function () {
-        const $character = document.getElementById('health-character');
-        console.log($character.innerText);
-        $character.innerText = enemy.damageHP + '/' + enemy.defaultHP;
-    },
-    renderHPLife: function () {
-        enemy.elHP.innerText = enemy.damageHP + ' / ' + enemy.defaultHP;
-    },
-    renderProgressbarHP: function () {
-        enemy.elProgressbar.style.width = enemy.damageHP + '%';
-    },
-
-    changeHP: function (count) {
-        if (enemy.damageHP < count) {
-            enemy.damageHP = 0;
-            alert('Бедный ' + enemy.name + ' проиграл бой!');
-            $btn2.disabled = enemy;
-
-        } else {
-            enemy.damageHP -= count;
-        }
-        renderHP();
-
-        const log = enemy === enemy ? generateLog(enemy, character) : generateLog(enemy, enemy);
-        const $p = document.createElement('p');
-        $p.innerText = log;
-        $logs.appendChild($p);
-    }
+function getElById(id) {
+    return document.getElementById(id);
 }
-const btnCountJolt=stop(6,$btn);
-$btn.addEventListener('click', function () {
-    btnCountJolt();
-    character.changeHP(random(60,20));
-});
-const btn2CountJolt=stop(6,$btn2);
-$btn2.addEventListener('click', function () {
-    btn2CountJolt();
-    enemy.changeHP(random(60,20));
-});
-function renderHP() {
-    character.renderHPLife();
-    character.renderProgressbarHP();
-    enemy.renderHPLife();
-    enemy.renderProgressbarHP();
-};
 
-const init = () => console.log('Start Game!');
+const btn = getElById('btn-kick');
+const btn2 = getElById('btn-kick-2');
+const $logs = document.querySelector('#logs');
+
+function countClicks(limitClicks) {
+    return () => limitClicks > 0 ? --limitClicks : limitClicks = 0;
+}
+
+const btn1_RemainClicks = countClicks(7);
+const btn2_RemainClicks = countClicks(7);
+
+const changeButtonName = (btn, clicks) => {
+    let btnName = btn.textContent.split(' ');
+
+    btnName.splice(btnName.indexOf('('), 1);
+
+    return `${btnName.join(' ')} (${clicks})`;
+}
+
+btn.addEventListener('click', function () {
+    console.log('Kick');
+
+    generateLog(player1, player2, player1.changeHP(random(30, 60)));
+    generateLog(player2, player1, player2.changeHP(random(30, 60)));
+
+    let remainClicks = btn1_RemainClicks();
+
+    btn.textContent = changeButtonName(btn, remainClicks);
+
+    if (!remainClicks) btn.disabled = true;
+
+    if (!player1.hp.current || !player2.hp.current) {
+        btn.disabled = true;
+        btn2.disabled = true;
+    }
+})
+
+btn2.addEventListener('click', function () {
+    console.log('Kick-2');
+
+    generateLog(player1, player2, player1.changeHP(random(40)));
+    generateLog(player2, player1, player2.changeHP(random(40)));
+
+    let remainClicks = btn2_RemainClicks();
+
+    btn2.textContent = changeButtonName(btn2, remainClicks);
+
+    if (!remainClicks) btn2.disabled = true;
+
+    if (!player1.hp.current || !player2.hp.current) {
+        btn.disabled = true;
+        btn2.disabled = true;
+    }
+})
+
+function generateLog(character, enemy, count) {
+    const logs = [
+        `${character.name} вспомнил что-то важное, но неожиданно ${enemy.name}, не помня себя от испуга, ударил в предплечье врага.`,
+        `${character.name} поперхнулся, и за это ${enemy.name} с испугу приложил прямой удар коленом в лоб врага.`,
+        `${character.name} забылся, но в это время наглый ${enemy.name}, приняв волевое решение, неслышно подойдя сзади, ударил.`,
+        `${character.name} пришел в себя, но неожиданно ${enemy.name} случайно нанес мощнейший удар.`,
+        `${character.name} поперхнулся, но в это время ${enemy.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника.`,
+        `${character.name} удивился, а ${enemy.name} пошатнувшись влепил подлый удар.`,
+        `${character.name} высморкался, но неожиданно ${enemy.name} провел дробящий удар.`,
+        `${character.name} пошатнулся, и внезапно наглый ${enemy.name} беспричинно ударил в ногу противника.`,
+        `${character.name} расстроился, как вдруг, неожиданно ${enemy.name} случайно влепил стопой в живот соперника.`,
+        `${character.name} пытался что-то сказать, но вдруг, неожиданно ${enemy.name} со скуки, разбил бровь сопернику.`
+    ];
+
+    let log = logs[random(logs.length) - 1];
+
+    const $p = document.createElement('p');
+    $p.innerText = `${log} (-${count}HP [${character.hp.current}/${character.hp.total}])`;
+    $logs.insertBefore($p, $logs.children[0]);
+    $logs.scrollTop = 0;
+}
+
+function init() {
+    console.log('Start Game!');
+
+    btn.textContent += ' (7)';
+    btn2.textContent += ' (7)';
+}
+
 init();
-
