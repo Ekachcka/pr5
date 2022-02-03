43c9d153eb3c880e7d76b832961074719607531a
Ekachcka <2331286yan@gmail.com>
2022-02-03 13:58:27 +0200

    Add 5-6

--- utils.js
+++ utils.js
@@ -1,3 +1,17 @@
-const random = (num) => Math.ceil(Math.random() * num);
+export function getElById(id) {
+    return document.getElementById(id);
+}
 
-export default random;
+export const random = (num) => Math.ceil(Math.random() * num);
+
+export function countClicks(limitClicks) {
+    return () => limitClicks > 0 ? --limitClicks : limitClicks = 0;
+}
+
+export const changeButtonName = (btn, clicks) => {
+    let btnName = btn.textContent.split(' ');
+
+    btnName.splice(btnName.indexOf('('), 1);
+
+    return `${btnName.join(' ')} (${clicks})`;
+}
